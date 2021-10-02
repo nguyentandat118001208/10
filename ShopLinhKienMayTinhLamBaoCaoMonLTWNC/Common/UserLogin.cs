901140656ca6cc1cc609dc96bc342cd47fc8e21d
datnguyenuhth <datnguyenuhth@gmail.com>
2021-10-02 16:59:59 +0700

    kho hieu

---
+++ ShopLinhKienMayTinhLamBaoCaoMonLTWNC/Common/UserLogin.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShopLinhKienMayTinhLamBaoCaoMonLTWNC.Common
+{
+    public class UserLogin
+    {
+    }
+}
